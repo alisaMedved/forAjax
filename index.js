8df5cd4d40bcac49e7c88ea37f892d1129b17a06
renlok87 <renlok87@gmail.com>
2019-08-26 20:53:34 +0500

    библиотека axios CRUD

--- index.js
+++ index.js
@@ -2,19 +2,70 @@ const resultBlock = document.querySelector("#result");
 const pageNumberEl = document.querySelector("#page-number");
 const clickMeButton = document.querySelector("#click-me");
 
+const getTasksButton = document.querySelector("#get-tasks");
+
+const deleteTaskButton = document.querySelector("#delete-task");
+const IDTaskText = document.querySelector("#ID-task");
+
+const createTaskButton = document.querySelector("#create-task");
+const titleTaskText = document.querySelector("#title-tasks");
+
+const updateTaskButton = document.querySelector("#update-task");
+const titleTaskUpdate = document.querySelector("#title-tasksUpdate");
+const IdTaskUpdate = document.querySelector("#ID-taskUpdate");
+
 clickMeButton.addEventListener("click", () => {
    const promise = getImages(pageNumberEl.value);
-   promise.then(onDataReceived);
+   promise.then(onImagesReceived);
 });
 
 
-function onDataReceived(mas) {
+getTasksButton.addEventListener("click", () => {
+    const promise = getTasks();
+    promise
+        .then(onTasksReceived);
+});
+
+deleteTaskButton.addEventListener("click", () => {
+    const promise = deleteTask(IDTaskText.value);
+});
+
+createTaskButton.addEventListener("click", () => {
+    const promise = createTask(titleTaskText.value);
+});
+
+updateTaskButton.addEventListener("click", () => {
+    const promise = updateTask(titleTaskUpdate.value, IdTaskUpdate.value);
+});
+
+function onImagesReceived(mas) {
     mas.forEach(el => {
         const img = document.createElement("img");
         img.src = el.thumbnail;
         document.querySelector("#result").appendChild(img);
     })
 }
+
+const onTasksReceived = (tasks) => {
+    const ulRes = document.querySelector("#result-tasks");
+    console.log(tasks);
+    tasks.map(task => {
+        const li = document.createElement("li");
+        li.innerHTML = task.title;
+        li.dataset.id = task.id;
+        ulRes.appendChild(li);
+    });
+};
+
+
+
+
+
+
+
+
+
+
 
 
 
--- service.js
+++ service.js
@@ -1,9 +1,4 @@
-// DAL - Data Access Layer
 
-function getImagesOld(pageNumber) {
-    const promise = $.ajax(`https://repetitora.net/api/JS/Images?page=${pageNumber}&count=2`);
-    return promise;
-}
 
 function getImages(pageNumber) {
     const promise = axios.get(`https://repetitora.net/api/JS/Images?page=${pageNumber}&count=2`);
@@ -13,6 +8,41 @@ function getImages(pageNumber) {
         })
 }
 
+function getTasks() {
+    const promise = axios.get("https://repetitora.net/api/JS/Tasks?widgetId=8907745&count=5");
+    return promise
+        .then((response) => {
+            return response.data;
+        })
+}
 
+const createTask = (title) => {
+    const promise = axios.post("https://repetitora.net/api/JS/Tasks", {
+        title,
+        widgetId: 8907745
+    });
+    return promise
+        .then((response) => {
+            return response.data.task;
+        })
+};
 
+const updateTask = (title, id) => {
+    const promise = axios.put("https://repetitora.net/api/JS/Tasks", {
+        title,
+        widgetId: 8907745,
+        taskId: id
+    });
+    return promise
+        .then((response) => {
+            return response.data;
+        })
+};
 
+const deleteTask = (id) => {
+    const promise = axios.delete(`https://repetitora.net/api/JS/Tasks?widgetId=8907745&taskId=${id}`);
+    return promise
+        .then((response) => {
+            return response.data;
+        })
+};
